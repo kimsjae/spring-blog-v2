51a069cdf88901da944b6dc2e9b870cdcd67e043
kimsjae <stwin755@gmail.com>
2024-03-19 15:51:15 +0900

    댓글 삭제

--- src/main/java/shop/mtcoding/blog/reply/ReplyService.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.blog.board.Board;
 import shop.mtcoding.blog.board.BoardJPARepository;
+import shop.mtcoding.blog.core.errors.exception.Exception403;
 import shop.mtcoding.blog.core.errors.exception.Exception404;
 import shop.mtcoding.blog.user.User;
 
@@ -23,5 +24,18 @@ public class ReplyService {
         Reply reply = reqDTO.toEntity(sessionUser, board);
 
         replyJPARepository.save(reply);
+    }
+
+    @Transactional
+    public void 댓글삭제(Integer replyId, Integer sessionUserId) {
+        Reply reply = replyJPARepository.findById(replyId)
+                        .orElseThrow(() -> new Exception404("없는 댓글을 삭제할 수 없어요."));
+
+        if (reply.getUser().getId() != sessionUserId) {
+            throw new Exception403("댓글을 삭제할 권한이 없어요.");
+        }
+
+
+        replyJPARepository.deleteById(replyId);
     }
 }
