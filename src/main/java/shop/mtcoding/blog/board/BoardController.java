4641732300b884274b283eeeca0af7ed0a815cde
kimsjae <stwin755@gmail.com>
2024-03-20 11:32:32 +0900

    메서드 변경, REST API 주소 변경

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -5,10 +5,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.blog.core.errors.exception.Exception403;
 import shop.mtcoding.blog.core.errors.exception.Exception404;
 import shop.mtcoding.blog.user.User;
@@ -23,25 +20,25 @@ public class BoardController {
     private final BoardJPARepository boardJAPRepository;
     private final HttpSession session;
 
-    // TODO: 글목록조회 API 필요
-    // TODO: 글조회 API 필요
-    // TODO: 글상세보기 API 필요
+    // TODO: 글목록조회 API 필요 -> @GetMapping("/")
+    // TODO: 글상세보기 API 필요 -> @GetMapping("/api/boards/{id}/detail")
+    // TODO: 글조회 API 필요 -> @GetMapping("/api/boards/{id}")
 
-    @PostMapping("/board/save")
+    @PostMapping("/api/boards")
     public String save(BoardRequest.SaveDTO reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         boardService.글쓰기(reqDTO, sessionUser);
         return "redirect:/";
     }
 
-    @PostMapping("/board/{id}/update")
+    @PutMapping("/api/boards/{id}")
     public String update(@PathVariable Integer id, BoardRequest.UpdateDTO reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         boardService.글수정(id, sessionUser.getId(), reqDTO);
         return "redirect:/board/" + id;
     }
 
-    @PostMapping("/board/{id}/delete")
+    @DeleteMapping("/api/boards/{id}")
     public String delete(@PathVariable Integer id) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         boardService.글삭제(id,sessionUser.getId());
--- src/main/java/shop/mtcoding/blog/core/config/WebMvcConfig.java
+++ src/main/java/shop/mtcoding/blog/core/config/WebMvcConfig.java
@@ -11,8 +11,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
-                .addPathPatterns("/board/**", "/user/**", "/reply/**")
-                .excludePathPatterns("/board/{id:\\d+}");
-
+                .addPathPatterns("/api/**"); // 인증필요X
     }
 }
